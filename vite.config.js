bccde9002512385ecb064ab7421fca822394455b
Evan Trad <evantrad@Evans-Air.lan>
2026-05-17 17:33:26 -0500

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/daily-shipped/',
 })
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/daily-shipped/",
-});
->>>>>>> 26e075b8c8250dec0c4935c39e04f1692126ecb9
